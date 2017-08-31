fe8b890719808e8fbf59e2e71545ce53d89bf763
Kyle Storey <kyle.r.storey@gmail.com>
2017-08-31 14:07:20 -0600

    Now Encodes and Decodes Messages

--- SecretAgent.java
+++ SecretAgent.java
@@ -9,14 +9,24 @@ public class SecretAgent {
 
   public SecretAgent() {
     luTable = new LookUpTable();
-    luTable.insert("bacon","Hello");
+    String[] code =   {"pastrami","tail", "shankle", "sausage", "ribeye", "bresaola","spicy", "jalapeno", "cow", "capicola", "pork","bacon", "picanha", "cupim", "hamburger", "kevin","beef"};
+    String[] decode = {"the","secret","keep","it","porky","police","this", "is", "a","message","from","Hello","it","is","top","that","way"};
+    for (int i = 0; i < 17; i++) {
+      luTable.insert(code[i],decode[i]);
+    }
     System.out.println(luTable.table[0].left + ":" + luTable.table[0].right);
 
   }
 
 	public String decode(String messageIn) {
-	  inMessage = messageIn;
-		return inMessage;
+    outMessage = "";
+    inMessage = messageIn;
+    Scanner in = new Scanner(messageIn);
+    while (in.hasNext()) {
+      String decoded = luTable.lookUp(in.next());
+      outMessage += decoded + " ";
+    }
+		return outMessage;
 	}
 
   private class CodePair {
@@ -59,10 +69,14 @@ public class SecretAgent {
 
     public String lookUp(String what) {
       for (CodePair pair : table) {
-        if (pair.left == what) return pair.right;
-        if (pair.right == what) return pair.left;
+        if (pair == null) break;
+        if (pair.left.equals(what)) {
+          return pair.right;
+        }
+        if (pair.right.equals(what)) return pair.left;
       }
-      return "ERROR: Not in Table";
+      System.out.println(what);
+      throw new NullPointerException();
     }
 
 
