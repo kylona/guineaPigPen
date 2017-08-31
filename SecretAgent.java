3164103a64d766a60759da9792f0edd509f94708
Kyle Storey <kyle.r.storey@gmail.com>
2017-08-31 12:56:11 -0600

    Inplement the LookUpTable and CodePair classes

--- SecretAgent.java
+++ SecretAgent.java
@@ -1,11 +1,72 @@
 
+import java.util.Scanner;
+
 public class SecretAgent {
 
-	String message;
+	String inMessage;
+  String outMessage;
+  LookUpTable luTable;
+
+  public SecretAgent() {
+    luTable = new LookUpTable();
+    luTable.insert("bacon","Hello");
+    System.out.println(luTable.table[0].left + ":" + luTable.table[0].right);
+
+  }
 
 	public String decode(String messageIn) {
-	  message = messageIn;
-		return message;
+	  inMessage = messageIn;
+		return inMessage;
 	}
+
+  private class CodePair {
+    String left;
+    String right;
+
+    public CodePair(String first, String second) {
+      left = first;
+      right = second;
+    }
+  }
+
+  private class LookUpTable {
+    int size;
+    int capacity;
+    CodePair[] table;
+
+    public LookUpTable() {
+      size = 0;
+      capacity = 100;
+      table = new CodePair[100];
+    }
+
+    public void insert(String first, String second) {
+      CodePair pair = new CodePair(first,second);
+      table[size] = pair;
+      size++;
+      if (size >= capacity) {
+        size = 0;
+        capacity = capacity * 2;
+        CodePair[] babyTable = new CodePair[capacity * 2];
+        for (CodePair babyPair : table) {
+          babyTable[size] = pair;
+          size++;
+        }
+        table = babyTable;
+      }
+
+    }
+
+    public String lookUp(String what) {
+      for (CodePair pair : table) {
+        if (pair.left == what) return pair.right;
+        if (pair.right == what) return pair.left;
+      }
+      return "ERROR: Not in Table";
+    }
+
+
+  }
+
 
 }
